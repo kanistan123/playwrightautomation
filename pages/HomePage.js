d28464e1bf65a892e250de6871136634266f0802
kanistan123 <kanijegatheesar9@gmail.com>
2026-03-30 06:28:11 +0530

    changes are made

---
+++ pages/HomePage.js
@@ -0,0 +1,9 @@
+exports.HomePage = class HomePage {
+  constructor(page) {
+    this.page = page;
+    this.url = "";
+    this.usernameInput = "#user-name";
+    this.passwordInput = "#password";
+    this.loginButton = "#login-button";
+  }
+};
---
+++ pages/LoginPage.js
@@ -0,0 +1,21 @@
+exports.LoginPage = class LoginPage {
+  constructor(Page) {
+    this.page = Page;
+    this.url = "https://demoblaze.com/";
+    this.login_bn = "#login2";
+    this.username_input = "#loginusername";
+    this.password_input = "#loginpassword";
+    this.login_btn = "button[text='Log in']";
+  }
+
+  async navigate() {
+    await this.page.goto(this.url);
+  }
+
+  async loginmethod(username, password) {
+    await this.page.click(this.login_bn);
+    await this.page.fill(this.username_input, username);
+    await this.page.fill(this.password_input, password);
+    await this.page.click(this.login_btn);
+  }
+};
